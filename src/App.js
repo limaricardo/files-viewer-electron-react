23efc153c2d52305a2b20d683d400608a9b4ea36
Ricardo <limaricardop@gmail.com>
2022-09-11 14:08:45 -0300

    Get Path and format size function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useMemo } from "react";
+import { FilesViewer } from "./FilesViewer";
 
-export const App = () => {
+const fs = window.require("fs");
+const pathModule = window.require("path");
+
+const { app } = window.require("@electron/remote");
+
+const formatSize = size => {
+  let i = Math.floor(Math.log(size) / Math.log(1024))
   return (
-    <div>App</div>
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    '' + 
+    ['B', 'kB', 'MB', 'GB', 'TB'][i]
   )
 }
+
+export const App = () => {
+  const [path, setPath] = useState(app.getAppPath());
+
+  const files = fs.readdirSync(path).map((file) => {
+    const stats = fs.statSync(pathModule.join(path, file))
+    return {
+      name: file,
+      size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
+      directory: stats.isDirectory()
+    }
+  });
+
+  return (
+    <div className="container mt-2">
+      <div className="form-group mt-4 mb-2"></div>
+    </div>
+  );
+};
